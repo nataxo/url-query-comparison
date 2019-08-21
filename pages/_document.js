9869165e5d7f884e6faa9dd9c52bb5c5664dc3b1
Natalia Kozlova <ikozlova.nata@gmail.com>
2019-08-22 00:22:52 +0300

    feat(ci): a11y + lightkeeper config

--- pages/_document.js
+++ pages/_document.js
@@ -1,30 +1,27 @@
-import Document, {Head, Main, NextScript} from 'next/document';
-import {getStyles} from 'reshadow';
+import Document, { Head, Main, NextScript } from "next/document";
+import { getStyles } from "reshadow";
 
-const mapStyles = styles => Object
-    .entries(styles)
-    .map(([id, code]) => (
-        <style id={id} dangerouslySetInnerHTML={{__html: code}} key={id} />
-    ));
+const mapStyles = styles =>
+  Object.entries(styles).map(([id, code]) => (
+    <style id={id} dangerouslySetInnerHTML={{ __html: code }} key={id} />
+  ));
 
 export default class MyDocument extends Document {
-    static async getInitialProps({renderPage}) {
-        const page = renderPage();
-        const style = getStyles();
-        return {...page, style};
-    }
+  static async getInitialProps({ renderPage }) {
+    const page = renderPage();
+    const style = getStyles();
+    return { ...page, style };
+  }
 
-    render() {
-        return (
-            <html>
-                <Head>
-                    {mapStyles(this.props.style.map)}
-                </Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
-            </html>
-        );
-    }
-};
+  render() {
+    return (
+      <html lang="en">
+        <Head>{mapStyles(this.props.style.map)}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
